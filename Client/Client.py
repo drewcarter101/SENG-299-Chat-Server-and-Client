41601ad93a09dfe07d95a52adcb2d632b5cda91d
Sharon <drewcarter101@hotmail.com>
2017-06-10 22:24:53 -0700

    Basic Client actions

--- Client/Client.py
+++ Client/Client.py
@@ -1,11 +1,17 @@
 
 import sys
+import os
 import re
 import json
+from formatter import ParseMessage
+
 
 credentials={}
 newUser=False
-
+credential_errors={"invalid_username": "Username must...", "invalid password": "Password must be...", "Invalid pairing": "The password entered does not match the username"}#fill in later
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+with open(os.path.join(__location__, "helpMsg.txt")) as myfile:
+    helpText=myfile.read()
 
 def usernameValid(username):
     #check if username is valid, return True or False
@@ -17,11 +23,26 @@ def usernameExists(username):
     return True
     #return Log_in.checkIfUsernameExists(username)
 
+def passwordValid(password):
+    return True
+
+def passwordMatch(username, password):
+    return True
+
+def peformAction(command, value):
+    if command=="set_alias":
+        credentials["username"]=value  
+    elif command== "help":
+        print helpText
+    elif command =="quit":
+        sys.exit()
+    return
+
 #sys.argv[1]
 #Prompt user for username
 print "Welcome"
 while True:
-    tempUser = raw_input("Please enter your chosen username\n\n")#wait for user to enter username
+    tempUser = raw_input("Please enter your chosen username: \n\n")#wait for user to enter username
     if usernameValid(tempUser):#check if user name is valid, if not valid start loop again
         if usernameExists(tempUser):#check if username exists in database
             credentials["username"]=tempUser #if it exists, set username
@@ -35,5 +56,34 @@ while True:
                 #signup user
                 break
     else:
-        print "Username must have..."#fill in later
+        print credential_errors["invalid_username"]
 
+#Check password
+while True:
+    tempPass = raw_input("please enter your password: " + credential_errors["invalid password"]+ "\n\n")
+    if passwordValid(tempPass):
+        if newUser:
+            credentials["password"]=tempPass
+            #Enter new user into database
+            print "Sign up complete! You are now logged in"
+            break
+        else:
+            #Check if password matches username
+            if passwordMatch(credentials["username"], tempPass):
+                credentials["password"]=tempPass
+                print "You are now logged in"
+                break
+            else:
+                print credential_errors["Invalid pairing"]
+    else:
+        print tempPass + " is not a valid password"
+  
+
+while True: #Main Program loop
+    input_list= raw_input("What do you want to do now?\n\n")
+    parser=ParseMessage(input_list.split(" "), credentials)
+    output= json.loads(parser.to_json())
+    if output["type"]=="client_command":
+        peformAction(output["command"], output["value"])
+    print(output)
+    break
--- Client/formatter.py
+++ Client/formatter.py
@@ -9,30 +9,43 @@ class ParseMessage():
     def send_command(self, commands, user_data):
         commands=list(filter(None, commands))
         data = {}
-        data["type"]="command"
+        if commands[0] in self.chatroom_commands[5:]:
+            data["type"]="client_command"
+        else:
+            data["type"]="command"
         data["command"] = commands[0]
+        
         if len(commands)>1: data["value"]=commands[1]
+        else: data["value"]=None
         data["user"] = user_data["username"]
         data["password"] = user_data["password"]
         json_data = json.dumps(data)
         return json_data
 
-    def send_message(self, message, user_data):
+    def send_message(self, message, input_list, errors, user_data):
         data = {}
         data["type"]="normal"
-        data["message"] = message
+        if message[0]=="/":
+            if input_list[0][1:] in self.chatroom_commands:
+                print errors["invalid_useOf_command"].format(input_list[0])
+            else:
+                print errors["invalid_command"].format(input_list[0])
+            data["message"] = ""
+        else:
+            data["message"] = message
         data["user"] = user_data["username"]
         data["password"] = user_data["password"]
         json_data = json.dumps(data)
         return json_data
 
     def __init__(self, input_list, user_data):
-        chatroom_commands =["join","create","set_alias","block", "unblock", "delete", "help", "quit"]
+        self.chatroom_commands =["join","create","block", "unblock", "delete","set_alias", "help", "quit"]
         formats={"/b": '\033[1m', "b/": '\033[0m', "/i": "/", "i/": "/", "/u": '\033[4m', "u/": '\033[0m', "/h": '\033[91m', "h/": '\033[0m', "/happy": u'\U0001f604', "/sad": u'\U0001F622', "/angry": u'\U0001F620', "/bored": u'\U0001F634', "/thumbsup": u'\U0001F44D', "/thumbsdown": u'\U0001F44E', "/highfive": u'\U0000270B'}
+        errors={"invalid_command" :"'{}' does not exist, Type /help for a list of chat commands", "invalid_useOf_command" :"Invalid arguments for '{}', Type /help for a list of chat commands"}
         input=" ".join([formats.get(item, item) for item in input_list])
 
-        pattern1="^/(?:({}))\[([A-Za-z0-9_]+)\]$".format("|".join(chatroom_commands))
-        pattern2="^/(?:({}))$".format("|".join(chatroom_commands[6:])) 
+        pattern1="^/(?:({})) ([A-Za-z0-9_]+)$".format("|".join(self.chatroom_commands))
+        pattern2="^/(?:({}))$".format("|".join(self.chatroom_commands[6:])) 
         general_pattern="(?:{}|{})".format(pattern1, pattern2)
 
         command_input=re.search(general_pattern, input)
@@ -41,17 +54,10 @@ class ParseMessage():
         if command_input is not None:
             self.output=self.send_command([command_input.group(1), command_input.group(2), command_input.group(3)], user_data)
         else:
-            self.output= self.send_message(input, user_data)
+            self.output= self.send_message(input, input_list, errors, user_data)
 
     def to_json(self):
         return self.output
-
-
-
-input_list=sys.argv[1:]
-user_d={"username":"Jeff", "password": "turtles"}
-output= json.loads(ParseMessage(input_list, user_d).to_json())
-print(output)
 
 
 
