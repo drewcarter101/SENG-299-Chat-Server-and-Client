4a7884ff1aef43fa4e109637db0080cbff6be455
CamSouthcott <camsouthcott@gmail.com>
2017-07-10 12:19:38 -0700

    ResponseFactory Added

---
+++ Chat/ResponseFactory.py
@@ -0,0 +1,83 @@
+import json
+
+
+class ResponseFactory:
+
+    RESPONSE_TYPE = 'responseType'
+    REQUEST_TYPE_MISSING = 'RequestTypeMissing'
+    REQUEST_FORMAT_ERROR = 'RequestFormatError'
+    OK = 'Ok'
+    DUPLICATE_USERNAME = 'DuplicateUsername'
+    INVALID_USERNAME = 'InvalidUsername'
+    INVALID_PASSWORD = 'InvalidPassword'
+    PARAMETERS_MISSING = 'ParametersMissing'
+    INVALID_CREDENTIALS = 'InvalidCredentials'
+    INVALID_MESSAGE = 'InvalidMessage'
+    BLOCKED = 'Blocked'
+    CHATROOM_DOES_NOT_EXIST = 'ChatroomDoesNotExist'
+    LAST_UPDATE = 'lastUpdate'
+    MESSAGES = 'messages'
+    DUPLICATE_CHATROOM = 'DuplicateChatroom'
+    USER_DOES_NOT_EXIST = 'UserDoesNotExist'
+    NOT_OWNER = 'NotOwner'
+    USER_NOT_ON_LIST = 'UserNotOnList'
+    USERNAME = 'username'
+    TEXT = 'text'
+
+    def __init__(self):
+        pass
+
+    def ok(self):
+        return json.dumps({self.RESPONSE_TYPE : self.OK})
+
+    def requestTypeMissing(self):
+        return json.dumps({self.RESPONSE_TYPE : self.REQUEST_TYPE_MISSING})
+
+    def requestFormatError(self):
+        return json.dumps({self.RESPONSE_TYPE : self.REQUEST_FORMAT_ERROR})
+
+    def duplicateUsername(self):
+        return json.dumps({self.RESPONSE_TYPE : self.DUPLICATE_USERNAME})
+
+    def invalidUsername(self):
+        return json.dumps({self.RESPONSE_TYPE : self.INVALID_USERNAME})
+
+    def invalidPassword(self):
+        return json.dumps({self.RESPONSE_TYPE : self.INVALID_PASSWORD})
+
+    def parametersMissing(self):
+        return json.dumps({self.RESPONSE_TYPE : self.PARAMETERS_MISSING})
+
+    def invalidCredentials(self):
+        return json.dumps({self.RESPONSE_TYPE : self.INVALID_CREDENTIALS})
+
+    def invalidMessage(self):
+        return json.dumps({self.RESPONSE_TYPE : self.INVALID_MESSAGE})
+
+    def blocked(self):
+        return json.dumps({self.RESPONSE_TYPE : self.BLOCKED})
+
+    def chatroomDoesNotExist(self):
+        return json.dumps({self.RESPONSE_TYPE : self.CHATROOM_DOES_NOT_EXIST})
+
+    def duplicateChatrooom(self):
+        return json.dumps({self.RESPONSE_TYPE : self.DUPLICATE_CHATROOM})
+
+    def userDoesNotExist(self):
+        return json.dumps({self.RESPONSE_TYPE : self.USER_DOES_NOT_EXIST})
+
+    def notOwner(self):
+        return json.dumps({self.RESPONSE_TYPE : self.NOT_OWNER})
+
+    def userNotOnList(self):
+        return json.dumps({self.RESPONSE_TYPE : self.USER_NOT_ON_LIST})
+
+    def returnMessages(self, lastUpdate, messages):
+        response = {self.RESPONSE_TYPE : self.OK, self.LAST_UPDATE : lastUpdate}
+        messagesJSON = []
+
+        for message in messages:
+            messagesJSON.append(json.loads({self.USERNAME : message.username, self.TEXT : message.text}))
+
+        response[self.MESSAGES] = messagesJSON
+        return json.dumps(response)
