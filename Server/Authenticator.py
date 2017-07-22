86cc7642161ecfe8554c648a284e0421dacf2278
Wenyun Liao <wenyun@Wenyuns-MacBook-Pro.local>
2017-07-22 12:57:04 -0700

    added one more parameter to Authenticator

--- Server/Authenticator.py
+++ Server/Authenticator.py
@@ -9,18 +9,28 @@ class Authenticator:
         "6b0f18a546857583252b8da92d3dfcbc811de51fc3ace445b3f972e35aba2f7f",
         "ec2-54-225-236-102.compute-1.amazonaws.com",5432)
 
-    def authenticateByID(self, userID):
-      
+    def authenticateByID(self, userID, pw):
+
         user= self.db.findByID(userID)
-        if user != None :
-            return True
+
+        if user != None:
+            if pw ==user.password :
+                return True
+            else:
+                return False 
         else:
             return False
       
     
 
-    def authenticateByName(self,username):
+    def authenticateByName(self,username,pw):
         user= self.db.findByName(username)
+
         if user !=None:
-            return True
-        return False 
+            if pw == user.password:
+                return True
+            else: 
+                return False
+        else:
+            return False 
+
