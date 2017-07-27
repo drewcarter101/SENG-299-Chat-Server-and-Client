80d0c6bc705fe0d81ac2aeb3222f376c59a132f7
Sharon Umute <drewcarter101@hotmail.com>
2017-07-26 17:02:40 -0700

    Update Start.py

--- Client/Start.py
+++ Client/Start.py
@@ -10,17 +10,17 @@ from ClientStateInfo import ClientStateInfo
 class Start():
 	def __init__(self):
 		#Help text
-        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-        with open(os.path.join(__location__, "helpMsg.txt")) as myfile:
+		__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+		with open(os.path.join(__location__, "helpMsg.txt")) as myfile:
 			self.helpText=myfile.read()
         
 		self.credential_errors={"Ok": "Success","InvalidUsername": "Usernames are alphanumeric and cannot be blank", "InvalidPassword": "Passwords are alphanumeric and cannot be blank", "Invalid_pairing": "Either the password or username entered is incorrect", "DuplicateUsername": "This user name already exists, please enter a valid username", "ParametersMissing" : "ParametersMissing"}
 		self.wrapper=ServerWrapper()
 		self.notTryingSignUp=True
 		
-        print "Welcome! Type '/quit' to exit or '/help' for assistance."
-        print "Login/sign-up below:\n"
-        #login/signup screen
+		print "Welcome! Type '/quit' to exit or '/help' for assistance."
+		print "Login/sign-up below:\n"
+		#login/signup screen
         while True:
             tempUser=raw_input("Please enter a username: " + self.credential_errors["InvalidUsername"]+ "\n")
             if tempUser=="/quit":
@@ -39,23 +39,24 @@ class Start():
 				if self.notTryingSignUp: print self.credential_errors["Invalid_pairing"]
 				response= raw_input("Press 's' to sign up as a new user or press any key to retry login\n")
 				if response== 's':
-                    self.notTryingSignUp=False
-                    print "Beginnng sign up process..."
-                    if self.wrapper.signup(tempUser, tempPass)=="Ok":
-                        print "Sign up complete, you are now logged in"
-                        break
-                    else:
-                        print self.credential_errors[self.wrapper.signup(tempUser, tempPass)]
-                elif response=="/quit":
-                    self.quit()
-                elif response=="/help":
-                    print self.helpText
-            else: 
-                print "Invalid entry"
+					self.notTryingSignUp=False
+					print "Beginnng sign up process..."
+					if self.wrapper.signup(tempUser, tempPass)=="Ok":
+						print "Sign up complete, you are now logged in"
+						break
+					else:
+						print self.credential_errors[self.wrapper.signup(tempUser, tempPass)]
+				elif response=="/quit":
+					self.quit()
+				elif response=="/help":
+					print self.helpText
+            else:
+				print "Invalid entry"
 				
-		self.csi=ClientStateInfo()
-		self.chat=Chat(self.csi.credentials, self.wrapper)
-		self.chat.run()
-				
+		
+        self.csi=ClientStateInfo()
+        self.chat=Chat(self.csi.credentials, self.wrapper)
+        self.chat.run()
+		
 	def quit(self):
-        sys.exit()
+		sys.exit()
