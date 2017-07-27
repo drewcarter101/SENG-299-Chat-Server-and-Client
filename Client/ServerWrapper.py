2cfb85f4964989cedad61259d418f1d6643784c1
WENYUNLIAO <wenyun@WENYUNLIAOs-MacBook-Pro.local>
2017-07-27 00:39:28 -0700

    exceptions are not tested

--- Client/ServerWrapper.py
+++ Client/ServerWrapper.py
@@ -1,93 +1,146 @@
 import socket 
 import json 
-
+from Message import Message 
 
 class ServerWrapper:
     ServerLocation ='localhost'
     Port= 9321
 
     def __init__(self):
-
         self.connect(self.Port)
     
 
     def connect(self,port):
         try:
             self.s=socket.socket()
-            host = socket.gethostname()
+           # host = socket.gethostname()
+            host=socket.gethostbyname(self.ServerLocation)
             self.s.connect((host,port))
         except IOError:
-            print({'error':'connectionFailed'})
-            return {'error':'connectionFailed'}
-
-
+            raise connectionFailedException 
+            
 
     def login(self,Username,Password):
-        
         data={'requestType':'login', 'username': Username, 'password': Password} 
-        return self.receive_and_parse(data)
+        reply=self.receive_and_parse(data)
+        outcome=reply["responseType"]
+        if outcome =="Ok":
+            
+            return reply["userID"]
+        
+        else:
+            
+            return self.exceptions(outcome)
         
 
     def signup(self,Username,Password):
     
         data = {'requestType':'signup', 'username': Username, 'password': Password}
-        return self.receive_and_parse(data)
-        
+        reply=self.receive_and_parse(data)
+        outcome=reply["responseType"]
+        if outcome =="Ok":
+            return reply["userID"]
+        else:
+            return self.exceptions(outcome)
     
     def send(self,UserID,Password,Chatroom,Message):
         
         data = {'requestType':'send', 'userID': UserID, 'password': Password,'chatroom':Chatroom,'message':Message}
-        return self.receive_and_parse(data)
-        
+        reply=self.receive_and_parse(data)  
+        outcome=reply["responseType"]
+        if outcome =="Ok":
+            return True
+        else:
+            return self.exceptions(outcome)
     
     def get(self,UserID,Password,Chatroom,LastUpdate=None):
-        
+    
         if LastUpdate is None:
             data = {'requestType':'get', 'userID': UserID, 'password': Password,'chatroom':Chatroom}
-            return self.receive_and_parse(data)
         else:
             data = {'requestType':'get', 'userID': UserID, 'password': Password,'chatroom':Chatroom,'lastUpdate':LastUpdate}
-            return self.receive_and_parse(data)
-        
+      
+        reply= self.receive_and_parse(data)
+        outcome=reply["responseType"]
+        if outcome =="Ok":
+
+            message_list=[]
+            for item in reply["messages"]:
+                message=Message(item["username"],item["text"])           
+                message_list.append(message) 
+            return message_list   
+        else:
+            return self.exceptions(outcome)
+           
     
     def set_alias(self,UserID,Password,NewUsername):
 
         data = {'requestType':'set_alias', 'userID': UserID, 'password': Password,'newUsername':NewUsername}
-        return self.receive_and_parse(data)
+        reply= self.receive_and_parse(data)
+        outcome=reply["responseType"]
+        if outcome =="Ok":
+             return True
+        else:
+            return self.exceptions(outcome)
         
 
     def join(self,UserID,Password,Chatroom):
         
         data = {'requestType':'join', 'userID': UserID, 'password': Password,'chatroom':Chatroom}
-        return self.receive_and_parse(data)
+        reply= self.receive_and_parse(data)
+        outcome=reply["responseType"]
+        if outcome =="Ok":
+             return True
+        else:
+            return self.exceptions(outcome)
         
 
     def create(self,UserID,Password,Chatroom):
         
         data = {'requestType':'create', 'userID': UserID, 'password': Password,'chatroom':Chatroom}
-        return self.receive_and_parse(data)
+        reply= self.receive_and_parse(data)
+        outcome=reply["responseType"]
+        if outcome =="Ok":
+             return True
+        else:
+            return self.exceptions(outcome)
         
 
     def block(self,UserID,Password,UserToBlock,Chatroom):
     
         data = {'requestType':'block', 'userID': UserID, 'password': Password,'chatroom':Chatroom,'userToBlock':UserToBlock}
-        return self.receive_and_parse(data)
+        reply= self.receive_and_parse(data)
+        outcome=reply["responseType"]
+        if outcome =="Ok":
+             return True
+        else:
+            return self.exceptions(outcome)
     
 
     def unblock(self,UserID,Password,UserToUnblock,Chatroom):
         
         data = {'requestType':'unblock', 'userID': UserID, 'password': Password,'chatroom':Chatroom,'userToUnblock':UserToUnblock}
-        return self.receive_and_parse(data)
+        reply= self.receive_and_parse(data)
+        outcome=reply["responseType"]
+        if outcome =="Ok":
+             return True
+        else:
+            return self.exceptions(outcome)
         
 
     def delete(self,UserID,Password,Chatroom):
         
         data = {'requestType':'delete', 'userID': UserID, 'password': Password,'chatroom':Chatroom}
-        return self.receive_and_parse(data)
+        reply= self.receive_and_parse(data)
+        outcome=reply["responseType"]
+        
+        if outcome =="Ok":
+             return True
+        else:
+            return self.exceptions(outcome)
         
 
     def receive_and_parse(self,data):
-        
         request=json.dumps(data)
         self.s.send(request)
         reply=json.loads(self.s.recv(2048))
@@ -95,3 +148,96 @@ class ServerWrapper:
         print reply
         return reply 
         
+
+    def exceptions(self,string):
+        if string =='requestTypeMissing':
+            raise requestTypeMissingException
+        elif string =='requestFormatError':
+            raise requestFormatErrorException 
+        elif string == 'duplicateUsername':
+            raise duplicateUsernameException
+        elif string == 'invalidUsername':
+            raise invalidUsernameException
+        elif string=='invalidPassword':
+            raise invalidPasswordException
+        elif string=='parametersMissing':
+            raise parametersMissingException 
+        elif string=='invalidCredentials':
+            raise invalidCredentialsException
+        elif string=='invalidMessage':
+            raise invalidMessageException
+        elif string=='chatroomDoesNotExist':
+            raise chatroomDoesNotExistException
+        elif string=='duplicateChatrooom':
+            raise duplicateChatrooomException
+        elif string=='userDoesNotExist':
+            raise userDoesNotExistException
+        elif string=='notOwner':
+            raise notOwnerException
+        elif string=='userNotOnList':
+            raise userNotOnListException 
+        elif string=='parameterFormatError':
+            raise parameterFormatErrorException
+        elif string=='invalidChatroom':
+            raise invalidChatroomException
+
+
+
+
+class connectionFailedException(Exception):
+    pass
+    
+class requestTypeMissingException(Exception):
+    pass
+    
+class requestFormatErrorException(Exception):
+    pass
+
+class duplicateUsernameException(Exception):
+    pass
+
+class invalidUsernameException(Exception):
+    pass
+
+class invalidPasswordException(Exception):
+    pass
+
+class parametersMissingException(Exception):
+    pass
+
+class invalidCredentialsException(Exception):
+    pass
+
+class invalidMessageException(Exception):
+    pass
+
+class chatroomDoesNotExistException(Exception):
+    pass
+
+class duplicateChatrooomException(Exception):
+    pass
+
+class userDoesNotExistException(Exception):
+    pass
+
+class notOwnerException(Exception):
+    pass
+
+class userNotOnListException(Exception):
+    pass
+
+class parameterFormatErrorException(Exception):
+    pass
+
+class invalidChatroomException(Exception):
+    pass
+
+
+#user=ServerWrapper()
+#user.signup('1234','4321')
+#user.send(0,'4321','general','ccc')
+
+
+#ser.get(0,'4321','general',1)
+
+
--- Client/test_ServerWrapper.py
+++ Client/test_ServerWrapper.py
@@ -1,6 +1,5 @@
 import unittest
-from ServerWrapper import ServerWrapper
-
+from ServerWrapper import *
 
 
 
@@ -8,52 +7,87 @@ class Test_Authenticator(unittest.TestCase):
     def test_connection(self):
         test= ServerWrapper()
         test.Port=1234
-        self.assertEqual(test.connect(test.Port),{'error':'connectionFailed'})
+        self.assertRaises(connectionFailedException,test.connect,test.Port)
 
-    def test_login(self):
+    def test_signup_login(self):
         test= ServerWrapper()
-        self.assertIsInstance(test.login('123','321'),dict)
-    
-
-    def test_signup(self):
-        test= ServerWrapper()
-        self.assertIsInstance(test.login('123','321'),dict)
+        self.assertIsInstance(test.signup('98','6789'),int)
+        test2=ServerWrapper()
+        self.assertIsInstance(test2.login('98','6789'),int)
     
     def test_send(self):
         test= ServerWrapper()
-        self.assertIsInstance(test.send(0,'321','chatroom','hello'),dict)
+        user_id=test.signup('c','d')
+        test2=ServerWrapper()
+        self.assertEqual(test2.send( user_id,'d','general','hello'),True)
     
     def test_get(self):
         test= ServerWrapper()
-        self.assertIsInstance(test.get(0,'321','chatroom'),dict)
+        user_id=test.signup('e','f')
+        test2=ServerWrapper()
+        test2.send( user_id,'f','general','hello')
+        test3=ServerWrapper()
+        self.assertIsInstance(test3.get( user_id,'f','general'),list) 
+        test4=ServerWrapper()
+        test4.send( user_id,'f','general','bye')
+        test5=ServerWrapper()
+        self.assertIsInstance(test5.get( user_id,'f','general',0),list)   
     
-    def test_get_one_more_parameter(self):
-        test= ServerWrapper()
-        self.assertIsInstance(test.get(0,'321','chatroom',0),dict)
     
     def test_set_alias(self):
         test= ServerWrapper()
-        self.assertIsInstance(test.set_alias(0,'321','1234'),dict)
+        user_id=test.signup('h','i')
+        test2=ServerWrapper()
+        self.assertEqual(test2.set_alias(user_id,'i','hh'),True)
+      
    
     def test_join(self):
         test= ServerWrapper()
-        self.assertIsInstance(test.join(0,'321','chatroom'),dict)
+        user_id1=test.signup('l','k')
+        test2=ServerWrapper()
+        user_id2=test2.signup('12','15')
+        test3=ServerWrapper()
+        test3.create(user_id1,'k','roomj')
+        test4=ServerWrapper()
+        self.assertEqual(test4.join( user_id2,'15','roomj'),True)
+       
    
     def test_create(self):
         test= ServerWrapper()
-        self.assertIsInstance(test.create(0,'321','chatroom'),dict)
+        user_id_create=test.signup('a','b')
+        test2=ServerWrapper()
+        self.assertEqual(test2.create(user_id_create,'b','roomc'),True)
 
     def test_block(self):
         test= ServerWrapper()
-        self.assertIsInstance(test.block(0,'321','12345','chatroom'),dict)
+        test.signup('lily','lon')
+        test1= ServerWrapper()
+        user_id_block=test1.signup('windy','bon')
+        test2= ServerWrapper()
+        test2.create(user_id_block,'bon','roomb')
+        test3=ServerWrapper()
+        self.assertEqual(test3.block(user_id_block,'bon','lily','roomb'),True)
 
     def test_unblock(self):
         test= ServerWrapper()
-        self.assertIsInstance(test.unblock(0,'321','12345','chatroom'),dict)
+        user_id=test.signup('vi','kon')
+        test1= ServerWrapper()
+        test1.signup('wow','pon')
+        test2= ServerWrapper()
+        test2.create(user_id,'kon','roomu')
+        test3=ServerWrapper()
+        test3.block(user_id,'kon','wow','roomu')
+        test4=ServerWrapper()
+        self.assertEqual(test4.unblock(user_id,'kon','wow','roomu'),True)
+
 
     def test_delete(self):
         test= ServerWrapper()
-        self.assertIsInstance(test.delete(0,'321','chatroom'),dict)
+        user_id=test.signup('willow','butter')
+        test2= ServerWrapper()
+        test2.create(user_id,'butter','roomd')
+        test3=ServerWrapper()
+        self.assertEqual(test3.delete( user_id,'butter','roomd'),True)
 
 
 
