0a24de4a777b7c6c80101e1f841e299d2b968ec9
Sharon Umute <drewcarter101@hotmail.com>
2017-07-26 17:30:52 -0700

    Update InputHandler.py

--- Client/InputHandler.py
+++ Client/InputHandler.py
@@ -71,7 +71,6 @@ class InputHandler():
 		
 
     def parser(self, input_list, user_data, chatroom):
-        self.wrapper=ServerWrapper()
         self.chatroom_commands =["join","create","block", "unblock", "delete","set_alias", "help", "quit"]
         #linux shell: formats={":b": '\033[1m', "b:": '\033[0m',":u": '\033[4m', "u:": '\033[0m', ":h": '\033[91m', "h:": '\033[0m', ":happy:": u'\U0001f604', ":sad:": u'\U0001F622', ":angry:": u'\U0001F620', ":bored:": u'\U0001F634', ":thumbsup:": u'\U0001F44D', ":thumbsdown:": u'\U0001F44E', ":highfive:": u'\U0000270B'}
         formats={":b": '\033[1m', "b:": '\033[22m ',":u": '__', "u:": '__', ":h": '\033[31m \033[1m', "h:": '\033[22m \033[39m ', ":happy:": ":)", ":sad:": ":(", ":angry:": ">:-(", ":bored:": "(-_-)" , ":thumbsup:": "(^ ^)b", ":thumbsdown:": "(- -)p", ":highfive:": "^_^/"}
